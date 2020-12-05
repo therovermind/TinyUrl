e02ca1202a7ddac5e3563a52f34b4bee245a41ce
therovermind <ankursinghal.dev@gmail.com>
2020-12-05 20:23:06 +0530

    adding database related stuff

--- src/main/java/com/ankur/TinyURL/Entity/TinyUrlEntity.java
+++ src/main/java/com/ankur/TinyURL/Entity/TinyUrlEntity.java
@@ -1,18 +1,21 @@
 package com.ankur.TinyURL.Entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity 
-@Table(name="STORE")
+@Table(name="URLS")
 public class TinyUrlEntity {
 	
 	@Id
 	@GeneratedValue
 	int id;
+	@Column(name="ORIGIN_URL")
 	String originalUrl;
+	@Column(name="SHORT_URL")
 	String smallUrl;
 	
 }
