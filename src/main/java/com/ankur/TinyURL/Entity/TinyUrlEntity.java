8a6dc8d38b8b9a5b589a2c0c1addc70817ecfff9
therovermind <ankursinghal.dev@gmail.com>
2021-02-03 18:52:31 +0530

    adding SequenceGenerator

--- src/main/java/com/ankur/TinyURL/Entity/TinyUrlEntity.java
+++ src/main/java/com/ankur/TinyURL/Entity/TinyUrlEntity.java
@@ -3,15 +3,18 @@ package com.ankur.TinyURL.Entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity 
 @Table(name="URLS")
+@SequenceGenerator(name="seq", initialValue=100001, allocationSize=1)
 public class TinyUrlEntity {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq")
 	private int id;
 	@Column(name="ORIGIN_URL")
 	private String originalUrl;
